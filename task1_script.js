d3fcc77a4497d5b1029fe286faf8fa831afcf138
cryingsarova <32322721+cryingsarova@users.noreply.github.com>
2019-03-19 13:13:11 +0300

    Add files via upload

--- task1_script.js
+++ task1_script.js
@@ -1,16 +1,23 @@
 
-function addRow(id){
+
+function addRow(){
 	var a = document.getElementsByTagName("input")[0].value;
 	var b = document.getElementsByTagName("input")[1].value;
 	var c = document.getElementsByTagName("input")[2].value;
 	
-    var tbody = document.getElementById(id).getElementsByTagName("TBODY")[0];
+    var tbody = document.getElementsByClassName("table1")[0].getElementsByTagName("TBODY")[0];
     var row = document.createElement("TR");
+	row.className+="row1";
+	row.addEventListener('click',deleteRow);
     var td1 = document.createElement("TD");
 	var td2 = document.createElement("TD");
+	td1.className+="td1"
+	td2.className+="td1"
 	var d = b*b - 4*a*c;
 	if(a==0){
-		alert('Первый коэффициент не должен быть равен 0!');
+		
+		document.getElementsByTagName("input")[0].value="";		
+		document.getElementsByTagName("input")[0].placeholder = "Коэффициент = 0!";
 	}
 	else if (d>=0){
 	var x1 = (-b + Math.sqrt(d))/2*a;
@@ -30,7 +37,15 @@ function addRow(id){
     row.appendChild(td2);
     tbody.appendChild(row);
 	}
+	
+	
   }
   
- 
-  
+function deleteRow(){
+	
+	console.log(this);
+	var parentElem = this.parentNode;
+	parentElem.removeChild(this);
+}
+
+
